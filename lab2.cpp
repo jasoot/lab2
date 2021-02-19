69b1e5edd7750fd0c5ba361c696856636480a045
jasoot <78027764+jasoot@users.noreply.github.com>
2021-02-19 19:42:05 +0200

    Add files via upload

---
+++ lab2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h>
+using namespace std
+
+;int main( ) 
+{
+ float a, h, S;
+ a=5; h=6;
+ ;S=a*h/2
+  ;cout<<"Input a and h:" <<endl
+   ;cin>>a>>h;
+ ;cout <<"S=" <<S << endl;
+ getch();
+ return 0;
+}
